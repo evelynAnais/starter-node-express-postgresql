2229040cfe8fc7e7d721bbe8767926f99dc94613
evelynAnais <evelyn.a.n.d.30@gmail.com>
2021-07-15 18:20:30 -0600

    added seed data

--- src/db/seeds/02-categories.js
+++ src/db/seeds/02-categories.js
@@ -1,13 +1,11 @@
-
+const categories = require('../fixtures/categories');
+const products = require('../fixtures/categories')
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex
+    .raw("TRUNCATE TABLE categories RESTART IDENTITY CASCADE")
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
+      return knex('categories').insert(categories);
     });
 };
